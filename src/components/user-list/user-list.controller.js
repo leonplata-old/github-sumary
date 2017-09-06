6ef1453ec40b00ecf54535e826e90ac84e27b308
Leon Plata <pcg.leon@gmail.com>
2017-09-06 12:19:37 -0400

    Fetching users on User List component

--- src/components/user-list/user-list.controller.js
+++ src/components/user-list/user-list.controller.js
@@ -1,2 +1,15 @@
 export default class UserListController {
+  constructor(UsersService) {
+    this.UsersService = UsersService;
+    this.users = [];
+  }
+
+  $onInit() {
+    this.UsersService.getUsers()
+      .then((users) => {
+        this.users = users;
+      });
+  }
 }
+
+UserListController.$inject = ['UsersService'];
